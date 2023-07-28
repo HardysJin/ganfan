88a6c44c9d6e7971881f52172fc8d11cdb19e841
Hardys Jin <hardyshongyijin@gmail.com>
2023-07-27 19:44:19 -0700

    fix multi-line order issue

--- src/components/MealOrders.vue
+++ src/components/MealOrders.vue
@@ -14,12 +14,14 @@ function toPrint(orders) {
 
 <template>
   <!-- <v-list density="compact" :items="toPrint(mealinfo.orders)"></v-list> -->
-  <v-list lines="one">
+  <v-list>
     <v-list-item
       v-for="item in toPrint(mealinfo.orders)"
+      :lines="item.subtitle.length > 25 ? 'two' : 'one'"
       :key="item.id"
       :title="item.title"
       :subtitle="item.subtitle"
+      style="padding: 4px 16px;"
     >
       <template #append>
         <v-btn density="compact" variant="text" icon="mdi-trash-can-outline" />
